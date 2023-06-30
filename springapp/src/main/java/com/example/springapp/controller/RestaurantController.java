894f3ce8e90002260b3a948934903a3d3eaa7792
anjalisingh11062002 <anjalisinghss2002@gmail.com>
2023-06-30 10:41:39 +0000

    restaurant

---
+++ springapp/src/main/java/com/example/springapp/controller/RestaurantController.java
@@ -0,0 +1,25 @@
+package com.example.springapp.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.springapp.model.Restaurant;
+import com.example.springapp.service.RestaurantService;
+
+@RestController
+@RequestMapping("/restaurant")
+public class RestaurantController {
+    private final RestaurantService restaurantService;
+
+    @Autowired
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Restaurant> createRestaurant(@RequestBody Restaurant restaurant) {
+        Restaurant createdRestaurant = restaurantService.createRestaurant(restaurant);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdRestaurant);
+    }
+}
---
+++ springapp/src/main/java/com/example/springapp/repository/RestaurantRepository.java
@@ -0,0 +1,8 @@
+package com.example.springapp.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.springapp.model.Restaurant;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+}
