23095cb6593152243d435ac0d0ff4bfb7446571d
anjalisingh11062002 <anjalisinghss2002@gmail.com>
2023-07-03 13:05:39 +0000

    restaurant

--- reactapp/src/setupTests.js
+++ reactapp/src/setupTests.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-=======
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
->>>>>>> 4c1ad5b7b044d1089657e638e562649e073e05b1
