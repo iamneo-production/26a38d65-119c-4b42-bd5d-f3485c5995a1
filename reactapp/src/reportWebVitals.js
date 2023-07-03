81dffa2b42909b2c6de9ce7a888226874db15a22
anjalisingh11062002 <anjalisinghss2002@gmail.com>
2023-07-03 15:20:21 +0000

    commit

--- reactapp/src/reportWebVitals.js
+++ reactapp/src/reportWebVitals.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
@@ -12,18 +11,3 @@ const reportWebVitals = onPerfEntry => {
 };
 
 export default reportWebVitals;
-=======
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
-
-export default reportWebVitals;
->>>>>>> 4c1ad5b7b044d1089657e638e562649e073e05b1
