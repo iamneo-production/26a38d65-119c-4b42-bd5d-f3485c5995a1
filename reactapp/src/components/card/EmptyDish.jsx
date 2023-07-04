f08947533b884b507532310b5a364474acb2bb4d
Avinash <70191708+vavinash992@users.noreply.github.com>
2023-07-04 22:34:39 +0530

    Add files via upload
    reactapp added

---
+++ reactapp/src/components/card/EmptyDish.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+  Card,
+  Stack,
+ Button,
+  CardContent,
+  TextField } from '@mui/material'
+import "./Card.css";
+import { CardHeader } from '@material-ui/core';
+const axios = require('axios').default;
+
+class EmptyDish extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishName: "",
+      imageUrl: "",
+      price : 0
+    }
+    this.addToMenu = this.addToMenu.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  addToMenu(event) {
+    event.preventDefault();
+    axios.post("/api/restaurant/addToMenu", {
+      restaurantId : this.props.currentUser.id,
+      dishName : this.state.dishName,
+      imageUrl : this.state.imageUrl,
+      price : this.state.price
+    }).then(
+      response => {
+        this.props.getAllDishes();
+        this.setState({dishName : "", imageUrl : "", price : 0})
+      }
+    ).catch(err => console.log(err));
+  }
+
+  handleChange(content) {
+    this.setState(content);
+  }
+
+  render() {
+    return this.props.currentUser ? (
+      <Card sx={{marginLeft:"500px",width:"400px",
+      marginTop:"50px",
+            justifyContent:"center",
+            alignContent:"center",
+            alignItems:"center"
+            }}>
+              <CardHeader title="Add Dish"/>
+                <CardContent >
+                    <form onSubmit={this.addToMenu}>
+                    <Stack spacing={3} >
+                        <TextField 
+                        InputProps={{
+                            style:{
+                                borderRadius:"15px",
+                                height:"45px",
+                            }
+                        }}
+                        variant='outlined'
+                        required
+                        autoFocus
+                        label="Dish Name"
+                        type="text"
+                        value={this.state.dishName}
+              onChange={event => this.handleChange({dishName: event.target.value})}
+                        />
+                        <TextField 
+                        variant='outlined'
+                        required
+                        label="Image url"
+                        type="text"
+                        InputProps={{
+                            style:{
+                                borderRadius:"10px",
+                                height:"45px"
+                            }
+                        }}
+                        value={this.state.imageUrl}
+              onChange={event => this.handleChange({imageUrl: event.target.value})}
+                        />
+                        <TextField 
+                        variant='outlined'
+                        InputProps={{
+                            style:{
+                                borderRadius:"10px",
+                                height:"45px"
+                            }
+                        }}
+                        required
+                        label="Price"
+                        type="text"
+                        value={this.state.price}
+              onChange={event => this.handleChange({price: event.target.value})}/>       
+                        <Button type="submit" variant="contained" size="small">Add</Button>
+                    </Stack>             
+                    </form>                   
+                </CardContent>
+            </Card>
+    ) : <div />;
+  }
+}
+
+export default EmptyDish;
---
+++ reactapp/src/components/driverView/mainpage/PendingOrders.jsx
@@ -0,0 +1,53 @@
+import { Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import './DriverHomes.css';
+import OrderCard from "../../card/OrderCard";
+const axios = require('axios').default;
+
+// const styles = {
+//   container: {
+//     display: 'flex',
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//     minHeight: '100vh',
+//     paddingRight: '20px',
+//     margin: '40px',
+//   },
+// };
+
+class PendingOrders extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      order: undefined
+    }
+    this.getActiveOrder = this.getActiveOrder.bind(this);
+  }
+
+  componentDidMount() {
+    this.getActiveOrder();
+  }
+
+  getActiveOrder() {
+    axios.get("/api/driver/myActiveOrder/" + this.props.currentUser.id).then(
+      response => {
+        this.setState({order: response.data});
+      }
+    ).catch(err => console.log(err));
+  }
+
+  render() {
+    return this.props.currentUser && this.state.order ? (
+      <div className= 'styling'>
+        <Grid container justify="space-evenly" spacing={3}>
+          <Grid item xs={5}>
+            <OrderCard order={this.state.order} userType={this.props.currentUser.type} getOrders={this.getActiveOrder} />
+          </Grid>
+        </Grid>
+      </div>
+    ) :
+    <div className= 'styling'> <Typography variant="h5"  style = {{marginBottom: '100px' }} className="styling"><i>You don't have any pending order...</i></Typography></div>
+  }
+}
+
+export default PendingOrders;
