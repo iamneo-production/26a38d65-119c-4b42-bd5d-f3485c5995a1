6dd47980f109c81b048f55b82149b96836fae67c
Avinash <70191708+vavinash992@users.noreply.github.com>
2023-07-04 22:19:40 +0530

    Add files via upload
    backend commit

---
+++ backend/src/main/java/com/example/backend/service/RestaurantService.java
@@ -0,0 +1,18 @@
+package com.example.backend.service;
+
+import com.example.backend.model.Dish;
+import com.example.backend.model.RestaurantInfo;
+import java.util.List;
+
+public interface RestaurantService {
+
+  int addDish(String id, Dish dish);
+
+  int removeDish(String id, Dish dish);
+
+  List<Dish> getAllDishes(String id);
+
+  RestaurantInfo getInformation(String id);
+
+  int updateInfo(String id, RestaurantInfo info);
+}
